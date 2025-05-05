bad333089d0460e2bc6c6ef5761797a650a8b057
Silapil <silapil@yandex.ru>
2025-05-05 23:20:31 +0300

    update

--- city_weather.py
+++ city_weather.py
@@ -1,9 +1,21 @@
-import requests
+import requests, sys, datetime
 from config.global_config import weather_cfg
+from json_yaml_csv_rw import file_rw
 
-city = str(input('Введите название города: '))
+##cities = [str(input(f'Введите название города {i}(5): ')) for i in range(1, 6)]
+cities = ['берлин', 'токио', 'Денвер', 'Пекин', 'брест']
 url = 'https://api.openweathermap.org/data/2.5/weather'
-params = weather_cfg(city)
+result = []
+result_columns = ['Время', 'Город', 'Температура']
+now_datetime = datetime.datetime.now().strftime('%d-%d-%Y %H_%M_%S')
 
-res = requests.get(url, params).json()
-print(f"Температура в г.{city.title()}: {res['main']['temp']}, C")
+try:
+    for city in cities:
+        params = weather_cfg(city)
+        res = requests.get(url, params).json()
+        result.append([now_datetime, f"{city.title()}:", f"{res['main']['temp']} C"])
+    file_rw(result, result_columns, 'csv_save', f'log/{now_datetime}.csv')
+except Exception as e:
+    print(e)
+    print(res)
+    sys.exit()
--- json_yaml_csv_rw.py
+++ json_yaml_csv_rw.py
@@ -1,33 +1,63 @@
-import yaml, json, pandas
-class MyClass:
-    data = ''
-    def data_info(self):
-        print(self.data)
+def file_rw(in_data, csv_columns, type_method, file_name):
+    import yaml, json, pandas, sys
+    from pathlib import Path
+    file_path = Path(file_name)
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    class MyClass:
+        data = in_data
+        def data_info(self):
+            print(self.data)
 
-    def yaml_read(file, self):
-        with open(file) as yaml_in:
-            self.data = yaml.load(yaml_in, Loader=yaml.FullLoader)
+        def text_append(file_name, self):
+            with open(file_name, 'a') as text_out:
+                for string in self.data:
+                    text_out.write('\n' + str(string))
 
-    def yaml_save(file, self):
-        with open(file, 'w') as yaml_out:
-            yaml.dump(self.data, yaml_out)
-            yaml_out.write(str(self.data))
+        def yaml_read(file_name, self):
+            with open(file_name) as yaml_in:
+                self.data = yaml.load(yaml_in, Loader=yaml.FullLoader)
 
-    def json_read(file, self):
-        with open(file) as json_in:
-            self.data = json.dumps(json.load(json_in), indent=4)
+        def yaml_save(file_name, self):
+            with open(file_name, 'w') as yaml_out:
+##                yaml.dump(self.data, yaml_out)
+                yaml_out.write(str(self.data) + '\n')
 
-    def json_save(file, self):
-        with open(file, 'w') as json_out:
-            json.dump(self.data, json_out)
-            json_out.write(str(self.data))
+        def json_read(file_name, self):
+            with open(file_name) as json_in:
+                self.data = json.dumps(json.load(json_in), indent=4)
 
-    def csv_read(file, self):
-        self.data = pandas.read_csv(file)
+        def json_save(file_name, self):
+            with open(file_name, 'w') as json_out:
+##                json.dump(self.data, json_out)
+                json_out.write(str(self.data) + '\n')
 
-    def csv_save(file, self):
-        pandas.DataFrame(self.data).to_csv(file, index=False)
+        def csv_read(file_name, self):
+            self.data = pandas.read_csv(file_name)
 
-my_file = MyClass()
-MyClass.csv_read('test.csv', my_file)
-MyClass.data_info(my_file)
+        def csv_save(file_name, csv_columns, self):
+            res = pandas.DataFrame(self.data)
+            res.columns = csv_columns
+            print(res)
+            res.to_csv(file_name, encoding='windows-1251', index=True)
+
+    my_file = MyClass()
+    if type_method == 'text_append':
+        MyClass.text_append(file_name, my_file)
+        sys.exit()
+    elif type_method == 'yaml_read':
+        MyClass.yaml_read(file_name, my_file)
+    elif type_method == 'yaml_save':
+        MyClass.yaml_save(file_name, my_file)
+    elif type_method == 'json_read':
+        MyClass.json_read(file_name, my_file)
+    elif type_method == 'json_save':
+        MyClass.json_save(file_name, my_file)
+    elif type_method == 'csv_read':
+        MyClass.csv_read(file_name, my_file)
+    elif type_method == 'csv_save':
+        MyClass.csv_save(file_name, csv_columns, my_file)
+        sys.exit()
+
+    MyClass.data_info(my_file)
+
+##file_rw(['1', '2', '3'], ['A', 'B', 'C'], 'csv_save', 'log/teeeeeeest.csv')
